3b52a3d20afd19b470becb527f6953990c7ebaa5
Ahmed Hamdy <hamdimn54@gmail.com>
2023-05-19 14:26:33 +0200

    [Components] add info component

---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { InfoService } from 'src/app/services/info.service';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss']
+})
+export class InfoComponent {
+  name: string;
+  title: string;
+  github: string;
+  linkedin: string;
+
+  constructor(private readonly infoService: InfoService) {
+    this.name = this.infoService.name;
+    this.title = this.infoService.title;
+    this.github = this.infoService.github;
+    this.linkedin = this.infoService.linkedin;
+  }
+
+  downloadResume(): void {
+    window.open('/assets/files/ahmed-hamdy-resume.pdf', '_blank');
+  }
+}
